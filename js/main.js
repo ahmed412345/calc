a4a464983ad43d19a536b887d3746721c78682b3
ahmed412345 <116723214+ahmed412345@users.noreply.github.com>
2024-11-02 19:08:25 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,34 +1,29 @@
 let field = document.getElementById('field');
-function one(a){
+function num(a){
     field.value += a;
-}
-let num1 =0;
-let num2 =0;
-let ask =0;
-function opera(b){
-    num1 = field.value;
-    field.value = '';
-    ask = b;
-}
-function equal(){
-    num2 = field.value;
-    if(ask == '+'){
-        field.value = Number(num1) + Number(num2);
-    }else if(ask == '-'){
-        field.value = Number(num1) - Number(num2);
-    }else if(ask == '*'){
-        field.value = Number(num1) * Number(num2);
-    }else if(ask == '/'){
-        if(num2 == 0){
-            alert('can not divide in zero');
-        }else{
-            field.value = Number(num1) / Number(num2);
-        }
-    }
 }
 function operaD(){
     field.value = field.value.slice(0,-1);
 }
 function operaDA(){
     field.value = '';
+}
+let equ = document.getElementById('equals')
+let sure = /^[0-9+\-*/.]+$/;
+function time(){
+    setTimeout(function(){
+        field.value = ''
+    },500)
+}
+equ.onclick = function(){
+    field.value = field.value.replace('÷','/').replace('×','*')
+    if(!sure.test(field.value)){
+        field.value = 'please write only number'
+        time();
+    }else if(eval(field.value) == 'Infinity'){
+        field.value = 'can not diven in zero'
+        time();
+    }else{
+        field.value = eval(field.value);
+    }
 }
